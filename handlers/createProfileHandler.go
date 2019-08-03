14742f7213b8236a51ac30aa38cdad7d0747b108
Trace Ohrt <teohrt18@gmail.com>
2019-08-02 23:43:03 -0700

    Decode request bodies

--- handlers/createProfileHandler.go
+++ handlers/createProfileHandler.go
@@ -1,15 +1,29 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/rs/zerolog"
+	"github.com/teohrt/cruddyAPI/entity"
 	"github.com/teohrt/cruddyAPI/service"
 )
 
 // TODO
 func CreateProfileHandler(svc service.Service) Handler {
 	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+		logger := zerolog.Ctx(r.Context())
+		decoder := json.NewDecoder(r.Body)
+
+		profile := new(entity.Profile)
+		if err := decoder.Decode(profile); err != nil {
+			logger.Debug().Err(err).Msg("Bad req body")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
 		fmt.Fprintf(w, "TODO")
 	}
 }

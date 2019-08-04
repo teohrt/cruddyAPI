10cfce67f35a26c33849182ed52c7fdc14d34618
Trace Ohrt <teohrt18@gmail.com>
2019-08-03 19:01:15 -0700

    updateProfile service

--- service/updateProfile.go
+++ service/updateProfile.go
@@ -6,7 +6,19 @@ import (
 	"github.com/teohrt/cruddyAPI/entity"
 )
 
-// TODO
 func (svc serviceImpl) UpdateProfile(ctx context.Context, profile entity.Profile) error {
+	_, err := svc.GetProfile(ctx, profile.ID)
+	if err != nil {
+		svc.Logger.Error().Err(err).Msg("UpdateProfile: GetProfile failed")
+		return err
+	}
+
+	_, err = svc.Client.UpsertItem(ctx, profile)
+	if err != nil {
+		svc.Logger.Printf("%v", profile.ID)
+		svc.Logger.Error().Err(err).Msg("UpdateProfile: UpsertItem failed")
+		return err
+	}
+
 	return nil
 }

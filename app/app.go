4cd08a7e90335b0d621dde9d01f62ffc5a5bf82b
Trace Ohrt <teohrt18@gmail.com>
2019-08-11 14:29:10 -0700

    Clearer env var naming

--- app/app.go
+++ app/app.go
@@ -16,7 +16,7 @@ import (
 )
 
 func Start() {
-	SERVER_PORT := os.Getenv("_LAMBDA_SERVER_PORT")
+	PORT := os.Getenv("SERVER_PORT")
 
 	config := dbclient.Config{}
 	env.Parse(&config)
@@ -24,12 +24,12 @@ func Start() {
 	v := validator.New()
 
 	r := mux.NewRouter()
-	s := r.PathPrefix("/cruddyAPI/v1").Subrouter()
+	s := r.PathPrefix("/cruddyAPI").Subrouter()
 	s.HandleFunc("/profiles", handlers.CreateProfile(svc, v)).Methods(http.MethodPost)
 	s.HandleFunc("/profiles/{id}", handlers.GetProfile(svc)).Methods(http.MethodGet)
 	s.HandleFunc("/profiles/{id}", handlers.UpdateProfile(svc, v)).Methods(http.MethodPut)
 	s.HandleFunc("/profiles/{id}", handlers.DeleteProfile(svc)).Methods(http.MethodDelete)
 
-	fmt.Println("Server listening on port :" + SERVER_PORT)
-	log.Fatal(http.ListenAndServe(":"+SERVER_PORT, r))
+	fmt.Println("Server listening on port :" + PORT)
+	log.Fatal(http.ListenAndServe(":"+PORT, r))
 }
